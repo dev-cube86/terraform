f82f5f961156a26565cee6357b162ba767008dd1
dev-cube86 <ddehtiar1986@gmail.com>
2024-05-17 21:22:27 +0000

    updated terraform configs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "GOOGLE_REGION" {
   type        = string
+  default = "us-central1-f"
   description = "GCP region to use"
 }
 
 variable "GOOGLE_PROJECT" {
   type        = string
+  default = "devops-training-419011"
   description = "GCP project name"
 }
